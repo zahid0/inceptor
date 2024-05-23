a36a8ec2d86acc0d3e8e183e910752ddd1a1cd9a
zahid0 <zahid904@gmail.com>
2024-05-23 20:10:53 +0530

    add fewer tools

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-import os
-
 from crewai import Agent, Crew, Task
-from crewai.process import Process
 
-from agents_utils import create_agent
 import tasks_utils
-
+from agents_utils import create_agent
 from models import AgentTaskList, Character
 from tools import get_tools
 
 
 def create_crew(agents, tasks, process=None, manager_llm=None, embedder=None):
     crew_kwargs = {
-        'agents': agents,
-        'tasks': tasks,
-        'verbose': 2,
-        'memory': True,
+        "agents": agents,
+        "tasks": tasks,
+        "verbose": 2,
+        "memory": True,
     }
     if embedder is not None:
-        crew_kwargs['embedder'] = embedder
+        crew_kwargs["embedder"] = embedder
     if process is not None:
-        crew_kwargs['process'] = process
+        crew_kwargs["process"] = process
     if manager_llm is not None:
-        crew_kwargs['manager_llm'] = manager_llm
+        crew_kwargs["manager_llm"] = manager_llm
     return Crew(**crew_kwargs)
 
 
 def main(goal, llm=None, embedder=None):
     tools = get_tools()
-    strategy_maker = create_agent(role='Strategy Maker', llm=llm)
-    critic = create_agent(role='Strategy Reviewer', llm=llm)
+    print(tools)
+    strategy_maker = create_agent(role="Strategy Maker", llm=llm)
+    critic = create_agent(role="Strategy Reviewer", llm=llm)
     plan_task = tasks_utils.create_plan_task(strategy_maker)
     review_task = tasks_utils.create_review_task(critic)
     planning_crew = create_crew(
-        agents=[strategy_maker, critic], tasks=[plan_task, review_task],
-        embedder=embedder
+        agents=[strategy_maker, critic],
+        tasks=[plan_task, review_task],
+        embedder=embedder,
     )
     tasks_json = planning_crew.kickoff(inputs={"goal": goal})
     tasks_list = AgentTaskList.parse_raw(tasks_json)
 
-    agent_creator = create_agent(role='Leader', llm=llm)
+    agent_creator = create_agent(role="Leader", llm=llm)
     team_creation_task = tasks_utils.create_team_task(agent_creator)
-    team_setup_crew = create_crew(agents=[agent_creator], tasks=[team_creation_task],
-        embedder=embedder)
+    team_setup_crew = create_crew(
+        agents=[agent_creator], tasks=[team_creation_task], embedder=embedder
+    )
 
     tasks = []
     agents = []
@@ -63,7 +62,7 @@ def main(goal, llm=None, embedder=None):
             role=character.role,
             goal=character.goal,
             backstory=character.backstory,
-            tools = tools,
+            tools=tools,
             allow_delegation=False,
             verbose=True,
             **llm_args,
@@ -78,9 +77,10 @@ def main(goal, llm=None, embedder=None):
         )
 
     master_crew = create_crew(
-        agents=agents, tasks=tasks, embedder=embedder,
-        #, process=Process.hierarchical, manager_llm=llm,
+        agents=agents,
+        tasks=tasks,
+        embedder=embedder,
+        # , process=Process.hierarchical, manager_llm=llm,
     )
 
     master_crew.kickoff()
-
--- tools.py
+++ tools.py
@@ -1,12 +1,10 @@
 import os
-from langchain_community.agent_toolkits import FileManagementToolkit
+
 from crewai_tools import ScrapeWebsiteTool, SerperDevTool
 
 
 def get_tools():
-    toolkit = FileManagementToolkit()  # If you don't provide a root_dir, operations will default to the current working directory
-    file_management_tools = toolkit.get_tools()
-    tool_list = file_management_tools + [ScrapeWebsiteTool()] 
-    if 'SERPER_API_KEY' in os.environ:
+    tool_list = [ScrapeWebsiteTool()]
+    if "SERPER_API_KEY" in os.environ:
         tool_list.append(SerperDevTool())
     return tool_list
